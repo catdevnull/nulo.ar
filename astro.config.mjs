c7ff2b99575dd08ebb4510a9a62730bb22e4ff74
Nulo <git@nulo.in>
2024-01-18 00:16:40 -0300

    inline stylesheets

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,13 +9,19 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), icon()],
+  build: { inlineStylesheets: "always" },
   markdown: {
-    remarkPlugins: [[wikiLinkPlugin, {
-      pageResolver: name => [slug(name)],
-      hrefTemplate: permalink => `/notas/${permalink}`
-    }]]
+    remarkPlugins: [
+      [
+        wikiLinkPlugin,
+        {
+          pageResolver: (name) => [slug(name)],
+          hrefTemplate: (permalink) => `/notas/${permalink}`,
+        },
+      ],
+    ],
   },
   redirects: {
     // TODO
-  }
+  },
 });
